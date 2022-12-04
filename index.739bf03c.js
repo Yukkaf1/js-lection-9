f37afdf901c014a45ae8ddf8e77a3a9548f886e9
Trunova Yulia <trunovaf11@gmail.com>
2022-12-04 12:53:20 +0100

    Updates

--- index.739bf03c.js
+++ index.739bf03c.js
@@ -532,7 +532,15 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"ebWYT":[function(require,module,exports) {
-console.log("Hallo Lection 9");
+//  console.log("Hallo Lection 9");
+const logMessege = ()=>{
+    console.log("Лог вызова callback через 3 сек");
+};
+console.log("До вызова setTimeout");
+setTimeout(()=>{
+    console.log("внутри callback для setTimeout");
+}, 2000);
+console.log("после вызова setTimeout");
 
 },{}]},["cVgJb","ebWYT"], "ebWYT", "parcelRequire2afd")
 
